990414c922966959fdb098b808a8bec0c73d50d1
HacAtac <jhackworth00@outlook.com>
2022-06-11 15:27:47 -0500

    set up postgresql django Listing model

--- jahre/settings.py
+++ jahre/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'jahre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jahredb',
+        'USER': 'postgres',
+        'PASSWORD': 'Chiefy21!',
+        'HOST': 'localhost',
     }
 }
 
