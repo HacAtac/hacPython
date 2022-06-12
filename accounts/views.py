a0f34704141f20eff2ac92d6c557b62d3f81cb01
HacAtac <jhackworth00@outlook.com>
2022-06-11 21:08:08 -0500

    Register & Login Templates + register/login method started

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render, redirect
 
 # Put your view methods here
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # Register the user
+        print('Registering the user')
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # Login the user
+        print('Registering the user')
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
