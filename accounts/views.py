bb5736cbcb4092be4c6dadabc5a6f2e2480483c2
HacAtac <jhackworth00@outlook.com>
2022-06-12 00:07:31 -0500

    Dynamic Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
 
 
 # Put your view methods here
@@ -71,4 +72,9 @@ def logout(request):
 
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+    context = {
+        'contacts': user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context)
