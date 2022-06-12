40d317ca848c9758cdaf4953e915b5d07062c937
HacAtac <jhackworth00@outlook.com>
2022-06-11 19:08:51 -0500

    Home & About Page is now Dynamic

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-listDate').filter(isPublished=True)[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hireDate')
+    # Get MVP reltor (realtor that bool is checked true)
+    mvpRealtors = Realtor.objects.all().filter(isMvp=True)
+    # Context to pass to the template
+    context = {
+        'realtors': realtors,
+        'mvpRealtors': mvpRealtors
+    }
+    return render(request, 'pages/about.html', context)
