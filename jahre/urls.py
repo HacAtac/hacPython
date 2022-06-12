1c93f232191e23752ebac5641aa509561fcee231
HacAtac <jhackworth00@outlook.com>
2022-06-12 04:58:56 -0500

    small

--- jahre/urls.py
+++ jahre/urls.py
@@ -1,4 +1,4 @@
-"""jahre URL Configuration
+"""btre URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
